b6210b54add7765f6ca1a592b3afed4c6e9dae77
Emma Willett <emmawv@gmail.com>
2020-12-30 18:23:39 +0100

    setup heroku

--- server/configs/mongoose.config.js
+++ server/configs/mongoose.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect(process.env.DB_LOCAL, { useNewUrlParser: true, useUnifiedTopology: true,  useFindAndModify: false })
+    .connect(process.env.DB_REMOTE, { useNewUrlParser: true, useUnifiedTopology: true,  useFindAndModify: false })
     .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
     .catch(err => console.error('Error connecting to mongo', err))
 
