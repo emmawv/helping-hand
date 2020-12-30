1663afe66e521b7aacc7e2268d038cdce7a594fa
Emma Willett <emmawv@gmail.com>
2020-12-30 19:36:53 +0100

    heroku client

--- client/src/service/psychologists.service.js
+++ client/src/service/psychologists.service.js
@@ -4,7 +4,7 @@ export default class PsychService {
 
     constructor() {
         this.apiHandler = axios.create({
-            baseURL: 'http://localhost:5000/api/psych',
+            baseURL: process.env.REACT_APP_API_URL,
             withCredentials: true
         })
     }
