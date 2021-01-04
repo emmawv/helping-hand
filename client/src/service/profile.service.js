33ee0fb0d9015819fb0225d04a1905d06c01a642
Emma Willett <emmawv@gmail.com>
2021-01-04 19:02:44 +0100

    fix url

--- client/src/service/profile.service.js
+++ client/src/service/profile.service.js
@@ -4,7 +4,7 @@ export default class ProfileService {
 
     constructor() {
         this.apiHandler = axios.create({
-            baseURL: 'http://localhost:5000/api',
+            baseURL: process.env.REACT_APP_API_URL,
             withCredentials: true
         })
     }
