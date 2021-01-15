9bf50d9af8a97100f0494c242dba1c3390e4a77f
Emma Willett <emmawv@gmail.com>
2021-01-15 13:08:07 +0100

    appointment card

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -97,7 +97,7 @@ const psychSchema = new Schema({
     },
     agesTreated: [{
         type: String,
-        enum: ['Adultos mayores (65+)', 'Adultos', 'Adolescentes(14 a 19)', 'Preadolescentes(11 a 13)', 'Niños(6 a 10)', 'Niños pequeños/preescolares(0 a 6)']
+        enum: ['Adultos mayores (65+)', 'Adultos', 'Adolescentes (14 a 19)', 'Preadolescentes (11 a 13)', 'Niños (6 a 10)', 'Niños pequeños/ preescolares (0 a 6)']
     }],
     meetType: [{
         type: String,
