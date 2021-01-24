6418dac6151e4e5dc420c36a513a0dcbc7b5d689
Emma Willett <emmawv@gmail.com>
2021-01-24 19:41:27 +0100

    delete doc

--- server/routes/profile.routes.js
+++ server/routes/profile.routes.js
@@ -5,28 +5,27 @@ const mongoose = require('mongoose')
 const User = require('../models/user.model')
 const Appointment = require('../models/appointments.model')
 
-router.put('/edit-psych', (req, res) => {
+// router.put('/edit-psych', (req, res) => {
 
-    User.Psych
-        .findByIdAndUpdate(req.user.id, req.body)
-        .then(response => res.json(response))
-        .catch(err => res.status(500).json(err))
-})
+//     User.Psych
+//         .findByIdAndUpdate(req.user.id, req.body)
+//         .then(response => res.json(response))
+//         .catch(err => res.status(500).json(err))
+// })
 
-router.put('/edit-patient', (req, res) => {
+// router.put('/edit-patient', (req, res) => {
 
-    User.Patient
-        .findByIdAndUpdate(req.user.id, req.body)
-        .then(response => res.json(response))
-        .catch(err => res.status(500).json(err))
-})
+//     User.Patient
+//         .findByIdAndUpdate(req.user.id, req.body)
+//         .then(response => res.json(response))
+//         .catch(err => res.status(500).json(err))
+// })
 
 router.put('/delete/:id', (req, res) => {
 
     Appointment
-        .updateMany({ userId: req.params.id }, { status: 'inactive' })
+        .updateMany({ $or: [{ userId: req.params.id }, { psychId: req.params.id }] }, { status: 'inactive' })
         .then(() => Appointment.find({ status:'inactive' }))
-        .then(response => response.map(elm => User.findByIdAndUpdate(elm.psychId, { notifications: true }, { new: true })))
         .then(() => User.findByIdAndUpdate(req.params.id, { accountStatus: 'inactive' }))
         .then(response => res.json(response))
         .catch(err => res.status(500).json(err))
